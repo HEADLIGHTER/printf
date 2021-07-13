54df8921dff24bb04b93b3fee3bdae5d83f7500a
Benny Bellatrix <Headlighterone@gmail.com>
2021-07-13 23:06:12 +0300

    37printf

--- ft_printf.c
+++ ft_printf.c
@@ -11,34 +11,34 @@
 /* ************************************************************************** */
 #include "printf.h"
 
-static void	write_char(char ch, size_t *c)
+static void	write_char(char c, size_t *count)
 {
 	write(1, &c, 1);
-	*c += 1;
+	*count += 1;
 }
 
 static int	hexpeps(char ch, va_list args, size_t *c)
 {
 	if (c == '%')
 	{
-		*c += 1;
+		*count += 1;
 		write(1, '%', 1);
 	}
 	else if (c == 'x' || c == 'X')
 	{
 		if (c == 'x')
-			dtohex(va_arg(args, unsigned int), "0123456789abcdef", c);
+			dtohex(va_arg(args, unsigned int), "0123456789abcdef", count);
 		else if (c == 'X')
-			dtohex(va_arg(args, unsigned int), "0123456789ABCDEF", c);
+			dtohex(va_arg(args, unsigned int), "0123456789ABCDEF", count);
 	}
 	else if (c == 'p')
-		pout(va_arg(args, void *), "0123456789abcdef", c);
+		pout(va_arg(args, void *), "0123456789abcdef", count);
 	if (c == '%' || c == 'x' || c == 'X' || c == 'p')
 		return (1);
 	return (0);
 }
 
-static int	chrstr(char ch, va_list args, size_t *c)
+static int	chrstr(char c, va_list args, size_t *count)
 {
 	char *strn;
 
@@ -46,32 +46,70 @@ static int	chrstr(char ch, va_list args, size_t *c)
 	{
 		strn = va_arg(args, char *);
 		if (strn == NULL)
+		{
+			pnull(count);
+			return (1);
+		}
+		*count += ft_strlen(strn);
+		printstr(strn);
 	}
+	else if (c == 'c')
+	{
+		*count += 1;
+		ft_putchar_fd(va_arg(args, int), 1);
+	}
+	if (c == 'c' || c == 's')
+		return (1);
+	return (0);
+}
+
+static int	deinui(char c, va_list args, count)
+{
+	char	*strn;
+	if (c == 'd' || c == 'i')
+	{
+		strn = ft_itoa(va_arg(args, int));
+		*count += ft_strlen(strn);
+		printstr(strn);
+		free(strn);
+	}
+	else if (c == 'u')
+	{
+		strn = uitoa(va_arg(args, unsigned int));
+		*count += ft_strlen(strn);
+		printstr(strn);
+		free(strn);
+	}
+	if (c == 'd' || c == 'i' || c == 'u')
+		return (1);
+	return (0);
 }
 
 int	ft_printf(const char *say_the_line_JC, ...)
 {
 	const char	*what_a_shame;
 	va_list		args;
-	size_t		c;
+	size_t		count;
 
 	what_a_shame = ft_strdup(say_the_line_JC);
 	va_start(args, what_a_shame);
+	count = 0;
 	while (*what_a_shame && what_a_shame)
 	{
-		if (*what_a_shame == %)
+		if (*what_a_shame == '%')
 		{
 			what_a_shame++;
-			if (!hexpeps(*what_a_shame, args, &c) && \
-					!chrstr(*what_a_shame, args, &c) && \
-					!deinui(*what_a_shame, args, &c))
-				write_char(*what_a_shame, &c);
+			if (!hexpeps(*what_a_shame, args, &count) && \
+					!chrstr(*what_a_shame, args, &count) && \
+					!deinui(*what_a_shame, args, &count))
+				write_char(*what_a_shame, &count);
 		}
 		else
-			write_char(*what_a_shame, &c);
+			write_char(*what_a_shame, &count);
 		if (*what_a_shame)
 			what_a_shame++;
 	}
 	va_end(args);
-	return (c);
+	free((char *)what_a_shame);
+	return (count);
 }
