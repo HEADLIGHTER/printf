dd7c67340b7ecbbd40a75d6de26420896d9c8a02
Benny Bellatrix <Headlighterone@gmail.com>
2021-07-14 00:20:35 +0300

    37header

--- ft_printf.h
+++ ft_printf.h
@@ -1,10 +1,14 @@
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-# include <unistd.h>
-# include <stdlib.h>
 # include <stdarg.h>
+# include "./libft/libft.h"
 
-int		ft_printf(const char say_the_line_JC);
+int		ft_printf(const char *say_the_line_JC, ...);
+void	ft_putstr(char *s);
+void	dtohex(unsigned int n, char *base, size_t *count);
+void	pout(void *arg, char *base, size_t *count);
+void	*ft_uitoa(unsigned int n);
+void	pnull(size_t *count);
 
 #endif
